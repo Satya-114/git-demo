dbbe13b3c0c5cbfa8905461eb2beea0aad82f56a
avinashmaddila00 <avinashmaddila00@gmail.com>
2021-05-10 14:36:06 +0530

    hii

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace git_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "cognine solutions";
+            Console.WriteLine(s);
+            Console.ReadKey();        }
+    }
+}
